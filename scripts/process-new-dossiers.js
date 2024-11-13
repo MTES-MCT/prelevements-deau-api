877ea855f2f768329521762be98f8c581cfaf35a
Théophile Merlière <theophile.merliere@gmail.com>
2024-11-13 15:45:08 +0100

    Store dossiers in mongodb

---
+++ scripts/process-new-dossiers.js
@@ -0,0 +1,27 @@
+import 'dotenv/config'
+import process from 'node:process'
+import mongo from '../lib/util/mongo.js'
+import {getAllDossiers} from '../lib/util/demarches-simplifies/index.js'
+
+const demarcheNumber = Number.parseInt(process.env.DS_DEMARCHE_NUMBER, 10)
+
+// Connect to MongoDB
+await mongo.connect()
+
+async function main() {
+  return getAllDossiers({
+    demarcheNumber,
+    first: 100,
+    state: 'en_instruction',
+    includeDossiers: true,
+    includeChamps: true
+  })
+}
+
+// Call the main function and ensure MongoDB is disconnected afterwards
+try {
+  await main()
+} finally {
+  // Disconnect from MongoDB
+  await mongo.disconnect()
+}
