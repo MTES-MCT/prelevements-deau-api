2cfcb1cbcb929ae13ded1523c3aa13d74aecb968
Théophile Merlière <theophile.merliere@gmail.com>
2024-11-13 15:45:42 +0100

    Dossiers API

---
+++ api.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import 'dotenv/config.js'
+
+import process from 'node:process'
+
+import express from 'express'
+import morgan from 'morgan'
+
+import mongo from './lib/util/mongo.js'
+
+import webRoutes from './lib/api/routes/web.js'
+
+// Connect to MongoDB
+await mongo.connect()
+
+const PORT = process.env.PORT || 5000
+const DEV = process.env.NODE_ENV !== 'production'
+
+const app = express()
+
+// Enable morgan logger (dev only)
+if (DEV) {
+  app.use(morgan('dev'))
+}
+
+app.use('/', webRoutes)
+
+// Start listening
+app.listen(PORT, () => {
+  console.log(`Start listening on port ${PORT}`)
+})
