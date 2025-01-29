27ec0a01ec819630f481a4e80ecf812bdf4b8d99
Guillaume <guillaume@livingdata.co>
2025-01-29 16:23:29 +0100

    Fix review: - Rename source for csv in .env - Add scalingo-postbuild script - Moves dotenv config - Removes urls in filenames

--- scripts/download-csv.js
+++ scripts/download-csv.js
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import {createWriteStream, existsSync, mkdirSync} from 'node:fs'
 import process from 'node:process'
 import {join, dirname} from 'node:path'
@@ -6,23 +7,21 @@ import {finished} from 'node:stream/promises'
 
 import got from 'got'
 
-import 'dotenv/config'
+const {CSV_SOURCE} = process.env
 
-const {SCALINGO_CSV_URL, SCALINGO_CSV_TOKEN} = process.env
-
-const urls = [
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/beneficiaire.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/document.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/exploitation-regle.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/exploitation.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/point-prelevement.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/regle.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/exploitation-usage.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/modalite-suivi.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/exploitation-modalite-suivi.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/exploitation-serie.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/serie-donnees.csv`,
-  `${SCALINGO_CSV_URL}${SCALINGO_CSV_TOKEN}/resultat-suivi.csv`
+const filenames = [
+  'beneficiaire.csv',
+  'document.csv',
+  'exploitation-regle.csv',
+  'exploitation.csv',
+  'point-prelevement.csv',
+  'regle.csv',
+  'exploitation-usage.csv',
+  'modalite-suivi.csv',
+  'exploitation-modalite-suivi.csv',
+  'exploitation-serie.csv',
+  'serie-donnees.csv',
+  'resultat-suivi.csv'
 ]
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
@@ -42,17 +41,17 @@ const downloadFile = async (url, filename) => {
 
     await finished(fileStream)
 
-    console.log(`Téléchargement terminé: ${filePath}`)
+    console.log(`Téléchargement terminé : ${filePath}`)
 
     return filePath
   } catch (error) {
-    console.error(`Échec du téléchargement de ${filename}:`, error.message)
+    console.error(`Échec du téléchargement de ${filename} :`, error.message)
     throw error
   }
 }
 
 try {
-  Promise.all(urls.map(url => downloadFile(url, url.split('/').pop())))
+  Promise.all(filenames.map(filename => downloadFile(`${CSV_SOURCE}/${filename}`, filename)))
 } catch (error) {
-  console.error('Échec du téléchargement:', error.message)
+  console.error('Échec du téléchargement :', error.message)
 }
