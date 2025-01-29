4d950acb07a2c6abaa37d8e25d4c2cd546e68d67
Jérôme Desboeufs <jerome@livingdata.co>
2025-01-29 22:53:04 +0100

    Move generic stuff to api.js

--- api.js
+++ api.js
@@ -5,10 +5,12 @@ import process from 'node:process'
 
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import mongo from './lib/util/mongo.js'
+import errorHandler from './lib/util/error-handler.js'
 
-import routes from './lib/api/routes/web.js'
+import routes from './lib/routes.js'
 
 // Connect to MongoDB
 await mongo.connect()
@@ -18,13 +20,22 @@ const DEV = process.env.NODE_ENV !== 'production'
 
 const app = express()
 
+// Enable CORS
+app.use(cors({origin: true, maxAge: 600}))
+
 // Enable morgan logger (dev only)
 if (DEV) {
   app.use(morgan('dev'))
 }
 
+// Setup JSON parsing
+app.use(express.json())
+
 app.use('/', routes)
 app.use('/api', routes) // Deprecated
+
+// Register error handler
+app.use(errorHandler)
 
 // Start listening
 app.listen(PORT, () => {
