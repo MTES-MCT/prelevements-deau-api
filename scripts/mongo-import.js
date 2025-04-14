39c0d2d8788554499b56077696536efafc36dada
Guillaume <guillaume@livingdata.co>
2025-04-14 15:07:09 +0200

    Import simplification

--- scripts/mongo-import.js
+++ scripts/mongo-import.js
@@ -1,349 +1,117 @@
-import {chain} from 'lodash-es'
-
 import mongo from '../lib/util/mongo.js'
 import * as storage from '../lib/models/internal/in-memory.js'
-import {getBnpe, getBssById} from '../lib/models/points-prelevement.js'
-
-async function updateExploitationsWithDocuments() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout des documments dans les exploitations')
-
-  const bulkOperations = storage.exploitationsDocuments.map(ed => ({
-    updateOne: {
-      filter: {id_exploitation: ed.id_exploitation},
-      update: {$addToSet: {documents: storage.indexedDocuments[ed.id_document]}}
-    }
-  }))
-
-  if (bulkOperations.length > 0) {
-    await mongo.db.collection('exploitations').bulkWrite(bulkOperations, {ordered: false})
-  }
-
-  console.log('\u001B[32;1m%s\u001B[0m', `\n=> ${bulkOperations.length} documents insérés\n\n`)
-}
-
-async function updateExploitationsWithRegles() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout des regles dans les exploitations')
-
-  const bulkOperations = storage.exploitationsRegles.map(er => ({
-    updateOne: {
-      filter: {id_exploitation: er.id_exploitation},
-      update: {$addToSet: {regles: storage.indexedRegles[er.id_regle]}}
-    }
-  }))
-
-  if (bulkOperations.length > 0) {
-    await mongo.db.collection('exploitations').bulkWrite(bulkOperations, {ordered: false})
-  }
-
-  console.log('\u001B[32;1m%s\u001B[0m', `\n=> ${bulkOperations.length} règles insérées\n\n`)
-}
-
-async function updateExploitationsWithModalites() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout des modalités dans les exploitations')
-
-  const bulkOperations = storage.exploitationModalites.map(er => ({
-    updateOne: {
-      filter: {id_exploitation: er.id_exploitation},
-      update: {$addToSet: {modalites: storage.indexedModalitesSuivis[er.id_modalite]}}
-    }
-  }))
-
-  if (bulkOperations.length > 0) {
-    await mongo.db.collection('exploitations').bulkWrite(bulkOperations, {ordered: false})
-  }
-
-  console.log('\u001B[32;1m%s\u001B[0m', `\n=> ${bulkOperations.length} modalités insérées\n\n`)
-}
-
-async function importCollection(data, collectionName) {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Importation des données : ' + collectionName)
-
-  if (!data || data.length === 0) {
-    console.error('Le fichier est vide !')
-    return
-  }
-
-  const collection = mongo.db.collection(collectionName)
-
-  console.log('\n=> Nettoyage de la collection...')
-  await collection.deleteMany()
-  console.log('...Ok !')
-
-  try {
-    const result = await collection.insertMany(data)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans ' + collectionName + '\n\n')
-  } catch (error) {
-    throw new Error('Erreur lors de l’importation des données : ' + error)
-  }
-}
-
-async function addBssToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout du BSS dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const bssData = points
-    .filter(point => point.id_bss)
-    .map(point => {
-      const bss = getBssById(point.id_bss)
-      return {
-        id_point: point.id_point,
-        bss: {
-          ...bss
-        }
-      }
-    })
-
-  for (const {id_point, bss} of bssData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point},
-        update: {
-          $set: {bss},
-          $unset: {id_bss: ''}
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function addBnpeToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout du BNPE dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const bnpeData = points
-    .filter(point => point.code_bnpe)
-    .map(point => {
-      const bnpe = getBnpe(point.code_bnpe)
-      return {
-        id_point: point.id_point,
-        bnpe
-      }
-    })
-
-  for (const {id_point, bnpe} of bnpeData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point},
-        update: {
-          $set: {bnpe},
-          $unset: {code_bnpe: ''}
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function addMeContinentaleBvToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout de meContinentalesBv dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const meContinentaleData = points
-    .filter(point => point.code_me_continentales_bv)
-    .map(point => {
-      const meContinentalesBv = storage.indexedMeContinentalesBv[point.code_me_continentales_bv]
-      return {
-        id_point: point.id_point,
-        meContinentalesBv
-      }
-    })
-
-  for (const {id_point, meContinentalesBv} of meContinentaleData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point},
-        update: {
-          $set: {meContinentalesBv},
-          $unset: {code_me_continentales_bv: ''}
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function addBvBdCarthageToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout de BvBdCarthage dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const bvBdCarthageData = points
-    .filter(point => point.code_bv_bdcarthage)
-    .map(point => {
-      const bvBdCarthage = storage.indexedBvBdCarthage[point.code_bv_bdcarthage]
-      return {
-        id_point: point.id_point,
-        bvBdCarthage
-      }
-    })
-
-  for (const {id_point, bvBdCarthage} of bvBdCarthageData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point},
-        update: {
-          $set: {bvBdCarthage},
-          $unset: {code_bv_bdcarthage: ''}
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function addMesoToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout de Meso dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const mesoData = points
-    .filter(point => point.code_meso)
-    .map(point => {
-      const meso = storage.indexedMeso[point.code_meso]
-      return {
-        id_point: point.id_point,
-        meso
-      }
-    })
-
-  for (const {id_point, meso} of mesoData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point},
-        update: {
-          $set: {meso},
-          $unset: {code_meso: ''}
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function addLIbelleCommuneToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout des libéllés de commune dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const mesoData = points
-    .filter(point => point.insee_com)
-    .map(point => {
-      const libelleCommune = storage.indexedLibellesCommunes[point.insee_com]
-      return {
-        id_point: point.id_point,
-        libelleCommune
-      }
-    })
-
-  for (const {id_point, libelleCommune} of mesoData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point},
-        update: {
-          $set: {libelleCommune: libelleCommune.nom}
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function addExploitationsInfosToPoints() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Ajout des informations sur les exploitations dans les points')
-  const points = await mongo.db.collection('points_prelevement').find().toArray()
-  const bulkOps = []
-
-  const exploitationsData = points
-    .map(point => {
-      const exploitations = storage.exploitationsIndexes.point[point.id_point]
-      return {
-        id_point: point.id_point,
-        exploitations: exploitations.map(e => e.id_exploitation),
-        usages: chain(exploitations).map('usages').flatten().uniq().value()
-      }
-    })
-
-  for (const e of exploitationsData) {
-    bulkOps.push({
-      updateOne: {
-        filter: {id_point: e.id_point},
-        update: {
-          $set: {
-            exploitations: e.exploitations,
-            usages: e.usages
-          }
-        }
-      }
-    })
-  }
-
-  if (bulkOps.length > 0) {
-    const result = await mongo.db.collection('points_prelevement').bulkWrite(bulkOps)
-    console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.modifiedCount + ' points modifiés\n\n')
-  }
-}
-
-async function removeUsageFromExploitations() {
-  console.log('\u001B[35;1;4m%s\u001B[0m', '• Suppression du champ usage des exploitations')
-
-  try {
-    const result = await mongo.db.collection('exploitations').updateMany(
-      {usage: {$exists: true}},
-      {$unset: {usage: ''}}
-    )
-
-    console.log('\u001B[32;1m%s\u001B[0m', `\n=> ${result.modifiedCount} exploitations modifiées`)
-    return result
-  } catch (error) {
-    console.error('\u001B[31mErreur:', error, '\u001B[0m')
-    throw error
-  }
-}
+import {
+  getDocumentFromExploitationId,
+  getExploitationsFromPointId,
+  getModalitesFromExploitationId,
+  getReglesFromExploitationId
+} from '../lib/models/exploitation.js'
 
 await mongo.connect()
 
-await importCollection(storage.beneficiaires, 'preleveurs')
-await importCollection(storage.exploitations, 'exploitations')
-await importCollection(storage.pointsPrelevement, 'points_prelevement')
+async function preparePoint(pointId) {
+  const point = storage.indexedPointsPrelevement[pointId]
 
-await addBssToPoints()
-await addBnpeToPoints()
-await addMeContinentaleBvToPoints()
-await addBvBdCarthageToPoints()
-await addMesoToPoints()
-await addLIbelleCommuneToPoints()
-await addExploitationsInfosToPoints()
-await updateExploitationsWithDocuments()
-await updateExploitationsWithRegles()
-await updateExploitationsWithModalites()
-await removeUsageFromExploitations()
+  point.bss = point.id_bss ? ({
+    id_bss: storage.indexedBss[point.id_bss].id_bss,
+    lien: storage.indexedBss[point.id_bss].lien_infoterre
+  }) : null
+
+  delete point.id_bss
+
+  point.bnpe = point.code_bnpe ? ({
+    point: storage.indexedBnpe[point.code_bnpe]?.code_point_prelevement,
+    lien: storage.indexedBnpe[point.code_bnpe]?.uri_ouvrage
+  }) : null
+
+  delete point.code_bnpe
+
+  point.meso = point.code_meso ? ({
+    code: storage.indexedMeso[point.code_meso].code,
+    nom: storage.indexedMeso[point.code_meso].nom_provis
+  }) : null
+
+  delete point.code_meso
+
+  point.meContinentalesBv = point.code_me_continentales_bv ? ({
+    code: storage.indexedMeContinentalesBv[point.code_me_continentales_bv].code_dce,
+    nom: storage.indexedMeContinentalesBv[point.code_me_continentales_bv].nom
+  }) : null
+
+  delete point.code_me_continentales_bv
+
+  point.bvBdCarthage = point.code_bv_bdcarthage ? ({
+    code: storage.indexedBvBdCarthage[point.code_bv_bdcarthage].code_cours,
+    nom: storage.indexedBvBdCarthage[point.code_bv_bdcarthage].toponyme_t
+  }) : null
+
+  delete point.code_bv_bdcarthage
+
+  point.commune = point.insee_com ? ({
+    code: point.insee_com,
+    nom: storage.indexedLibellesCommunes[point.insee_com].nom
+  }) : null
+
+  delete point.insee_com
+
+  const exploitations = await getExploitationsFromPointId(pointId)
+  point.exploitations = exploitations.map(e => e.id_exploitation)
+
+  return point
+}
+
+async function prepareExploitation(idExploitation) {
+  const exploitation = storage.indexedExploitations[idExploitation]
+
+  exploitation.regles = await getReglesFromExploitationId(idExploitation)
+  exploitation.documents = await getDocumentFromExploitationId(idExploitation)
+  exploitation.modalites = await getModalitesFromExploitationId(idExploitation)
+
+  delete exploitation.usage
+
+  return exploitation
+}
+
+async function importPoints() {
+  console.log('\n\u001B[35;1;4m%s\u001B[0m', '=> Importation des données points_prelevement')
+
+  console.log('\n=> Nettoyage de la collection points_prelevement...')
+  await mongo.db.collection('points_prelevement').deleteMany()
+  console.log('...Ok !')
+
+  const points = await Promise.all(storage.pointsPrelevement.map(({id_point}) => preparePoint(id_point)))
+  const result = await mongo.db.collection('points_prelevement').insertMany(points)
+
+  console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans la collection points_prelevement\n\n')
+}
+
+async function importExploitations() {
+  console.log('\n\u001B[35;1;4m%s\u001B[0m', '=> Importation des données exploitations')
+
+  console.log('\n=> Nettoyage de la collection exploitations...')
+  await mongo.db.collection('exploitations').deleteMany()
+  console.log('...Ok !')
+
+  const exploitations = await Promise.all(storage.exploitations.map(({id_exploitation}) => prepareExploitation(id_exploitation)))
+  const result = await mongo.db.collection('exploitations').insertMany(exploitations)
+
+  console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans la collection exploitations\n\n')
+}
+
+async function importPreleveurs() {
+  console.log('\n\u001B[35;1;4m%s\u001B[0m', '=> Importation des données preleveurs')
+
+  console.log('\n=> Nettoyage de la collection preleveurs...')
+  await mongo.db.collection('preleveurs').deleteMany()
+  console.log('...Ok !')
+
+  const result = await mongo.db.collection('preleveurs').insertMany(storage.beneficiaires)
+
+  console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans la collection preleveurs\n\n')
+}
+
+await importPoints()
+await importExploitations()
+await importPreleveurs()
 
 await mongo.disconnect()
