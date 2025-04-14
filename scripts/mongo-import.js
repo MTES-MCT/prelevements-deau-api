a66b574b7a1a379b38e14722741c2288e12cadba
GllmR <56537238+GllmR@users.noreply.github.com>
2025-04-14 15:52:39 +0200

    Importe des données dans mongo et création des objets (#21)
    * Update mongo indexes
    * Removes import BSS & BNPE
    * Import preleveurs/exploitations/points_prelevement

--- scripts/mongo-import.js
+++ scripts/mongo-import.js
@@ -1,59 +1,120 @@
+import chain from 'lodash-es'
 import mongo from '../lib/util/mongo.js'
 import * as storage from '../lib/models/internal/in-memory.js'
-
-async function importBSS(bss) {
-  console.log('Importation des données BSS...')
-  if (!bss || bss.length === 0) {
-    console.error('Le fichier est vide !')
-    return
-  }
-
-  const collection = mongo.db.collection('bss')
-
-  console.log('Nettoyage de la collection...')
-  await collection.deleteMany({})
-
-  const documents = bss.map(b => ({
-    id_bss: b.id_bss,
-    lien_infoterre: b.lien_infoterre
-  }))
-
-  try {
-    const result = await collection.insertMany(documents)
-    console.log('=> ' + result.insertedCount + ' documents BSS insérés')
-  } catch (error) {
-    throw new Error('Erreur lors de l’importation : ' + error)
-  }
-}
-
-async function importBNPE(bnpe) {
-  console.log('Importation des données BNPE...')
-  if (!bnpe || bnpe.length === 0) {
-    console.error('Le fichier est vide !')
-    return
-  }
-
-  const collection = mongo.db.collection('bnpe')
-
-  console.log('Nettoyage de la collection...')
-  await collection.deleteMany({})
-
-  const documents = bnpe.map(b => ({
-    code_point_prelevement: b.code_point_prelevement,
-    uri_ouvrage: b.uri_ouvrage
-  }))
-
-  try {
-    const result = await collection.insertMany(documents)
-    console.log('=> ' + result.insertedCount + ' documents BNPE insérés')
-  } catch (error) {
-    throw new Error('Erreur lors de l’importation : ' + error)
-  }
-}
+import {
+  getDocumentFromExploitationId,
+  getExploitationsFromPointId,
+  getModalitesFromExploitationId,
+  getReglesFromExploitationId
+} from '../lib/models/exploitation.js'
 
 await mongo.connect()
 
-await importBSS(storage.bss)
-await importBNPE(storage.bnpe)
+async function preparePoint(pointId) {
+  const point = storage.indexedPointsPrelevement[pointId]
+
+  point.bss = point.id_bss ? ({
+    id_bss: storage.indexedBss[point.id_bss].id_bss,
+    lien: storage.indexedBss[point.id_bss].lien_infoterre
+  }) : null
+
+  delete point.id_bss
+
+  point.bnpe = point.code_bnpe ? ({
+    point: storage.indexedBnpe[point.code_bnpe]?.code_point_prelevement,
+    lien: storage.indexedBnpe[point.code_bnpe]?.uri_ouvrage
+  }) : null
+
+  delete point.code_bnpe
+
+  point.meso = point.code_meso ? ({
+    code: storage.indexedMeso[point.code_meso].code,
+    nom: storage.indexedMeso[point.code_meso].nom_provis
+  }) : null
+
+  delete point.code_meso
+
+  point.meContinentalesBv = point.code_me_continentales_bv ? ({
+    code: storage.indexedMeContinentalesBv[point.code_me_continentales_bv].code_dce,
+    nom: storage.indexedMeContinentalesBv[point.code_me_continentales_bv].nom
+  }) : null
+
+  delete point.code_me_continentales_bv
+
+  point.bvBdCarthage = point.code_bv_bdcarthage ? ({
+    code: storage.indexedBvBdCarthage[point.code_bv_bdcarthage].code_cours,
+    nom: storage.indexedBvBdCarthage[point.code_bv_bdcarthage].toponyme_t
+  }) : null
+
+  delete point.code_bv_bdcarthage
+
+  point.commune = point.insee_com ? ({
+    code: point.insee_com,
+    nom: storage.indexedLibellesCommunes[point.insee_com].nom
+  }) : null
+
+  delete point.insee_com
+
+  const exploitations = await getExploitationsFromPointId(pointId)
+  point.exploitations = exploitations.map(e => e.id_exploitation)
+
+  point.usages = chain(exploitations).map('usages').flatten().uniq().value()
+
+  return point
+}
+
+async function prepareExploitation(idExploitation) {
+  const exploitation = storage.indexedExploitations[idExploitation]
+
+  exploitation.regles = await getReglesFromExploitationId(idExploitation)
+  exploitation.documents = await getDocumentFromExploitationId(idExploitation)
+  exploitation.modalites = await getModalitesFromExploitationId(idExploitation)
+
+  delete exploitation.usage
+
+  return exploitation
+}
+
+async function importPoints() {
+  console.log('\n\u001B[35;1;4m%s\u001B[0m', '=> Importation des données points_prelevement')
+
+  console.log('\n=> Nettoyage de la collection points_prelevement...')
+  await mongo.db.collection('points_prelevement').deleteMany()
+  console.log('...Ok !')
+
+  const points = await Promise.all(storage.pointsPrelevement.map(({id_point}) => preparePoint(id_point)))
+  const result = await mongo.db.collection('points_prelevement').insertMany(points)
+
+  console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans la collection points_prelevement\n\n')
+}
+
+async function importExploitations() {
+  console.log('\n\u001B[35;1;4m%s\u001B[0m', '=> Importation des données exploitations')
+
+  console.log('\n=> Nettoyage de la collection exploitations...')
+  await mongo.db.collection('exploitations').deleteMany()
+  console.log('...Ok !')
+
+  const exploitations = await Promise.all(storage.exploitations.map(({id_exploitation}) => prepareExploitation(id_exploitation)))
+  const result = await mongo.db.collection('exploitations').insertMany(exploitations)
+
+  console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans la collection exploitations\n\n')
+}
+
+async function importPreleveurs() {
+  console.log('\n\u001B[35;1;4m%s\u001B[0m', '=> Importation des données preleveurs')
+
+  console.log('\n=> Nettoyage de la collection preleveurs...')
+  await mongo.db.collection('preleveurs').deleteMany()
+  console.log('...Ok !')
+
+  const result = await mongo.db.collection('preleveurs').insertMany(storage.beneficiaires)
+
+  console.log('\u001B[32;1m%s\u001B[0m', '\n=> ' + result.insertedCount + ' documents insérés dans la collection preleveurs\n\n')
+}
+
+await importPoints()
+await importExploitations()
+await importPreleveurs()
 
 await mongo.disconnect()
