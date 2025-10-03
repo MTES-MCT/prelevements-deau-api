3e9e23a1bfc47520d1da8a155f9abdd8a4077258
Jérôme Desboeufs <jerome@livingdata.co>
2025-10-03 15:47:03 +0200

    Drop unused function

--- packages/timeseries-parsers/lib/validate.js
+++ packages/timeseries-parsers/lib/validate.js
@@ -1,18 +1,3 @@
-// Fonction pour valider si une date est dans la période du formulaire
-export function validateDateInPeriod(dateValue, {startDate, endDate}) {
-  dateValue = dateValue.toISOString
-    ? dateValue.toISOString().slice(0, 10)
-    : dateValue.slice(0, 10)
-
-  if (startDate && endDate && (dateValue < startDate || dateValue > endDate)) {
-    const error = new Error('Date en dehors de la période indiquée')
-    error.explanation = `La date ${dateValue} n'est pas comprise entre le ${startDate} et le ${endDate}.`
-    throw error
-  }
-
-  return dateValue
-}
-
 // Fonction pour valider qu'une valeur est numérique et positive
 export function validateNumericValue(value) {
   if (value !== undefined && value !== null && value !== '') {
