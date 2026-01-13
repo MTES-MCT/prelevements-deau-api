ecedf6707be68bafc61d6f81159f2cc3305e7f4d
Samy GHRIBI <ghribi.samy@gmail.com>
2026-01-13 16:21:29 +0100

    Add healtcheck on worker

--- worker.js
+++ worker.js
@@ -1,11 +1,35 @@
 #!/usr/bin/env node
 import 'dotenv/config'
+import http from 'node:http'
 import mongo from './lib/util/mongo.js'
 import {startWorkers} from './lib/queues/workers.js'
 import {startScheduler} from './lib/queues/scheduler.js'
+
+let ready = false
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    if (ready) {
+      res.writeHead(200)
+      res.end('ok')
+    } else {
+      res.writeHead(503)
+      res.end('starting')
+    }
+    return
+  }
+
+  res.writeHead(404)
+  res.end()
+})
+
+server.listen(8080, () => {
+  console.log('Healthcheck server listening on port 8080')
+})
 
 await mongo.connect()
 await startScheduler()
 startWorkers()
 
+ready = true
 console.log('Workers started')
