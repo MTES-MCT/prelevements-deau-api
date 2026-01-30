616084adf6362f393d8f1d3819bfb4661c3104f9
Samy GHRIBI <samy.ghribi.ext@beta.gouv.fr>
2026-01-30 15:51:14 +0100

    Add Sentry integration

--- worker.js
+++ worker.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 import 'dotenv/config'
+import './instrument.js'
 import mongo from './lib/util/mongo.js'
 import {startWorkers} from './lib/queues/workers.js'
 import {startScheduler} from './lib/queues/scheduler.js'
+import * as Sentry from '@sentry/node'
+
+Sentry.setTag('service', 'worker')
 
 await mongo.connect()
 await startScheduler()
